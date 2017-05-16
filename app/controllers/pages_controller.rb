2114af4de38c079d1f5f8ceae12e5c331f49b5cb
Jordan Thomas <hello@jordanthom.as>
2017-05-16 13:01:44 +0900

    Add global ranking to index view

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,18 @@
 class PagesController < ApplicationController
-  def index
-  end
+
+	def index
+
+		# For global rank:
+		# Add all rankings, girl with lowest number = highest rank
+
+		girls = Hash.new(0) # Initialise hash map
+
+		# Get each ranking
+		Ranking.find_each do |ranking|
+			girls[ranking.girl_id] += ranking.rank;
+		end
+		
+		@globalRanking = girls.sort_by { |girl_id, rank| rank }
+
+	end
 end

f21e2bba9cf4ba9e049e06dd0795eb69f823c72c
Jordan Thomas <hello@jordanthom.as>
2017-05-16 16:56:52 +0900

    Add test for HTTP status if signed in/out

--- spec/requests/pages_spec.rb
+++ spec/requests/pages_spec.rb
@@ -2,9 +2,15 @@ require 'rails_helper'
 
 RSpec.describe "Pages", type: :request do
   describe "GET /pages" do
-    it "works! (now write some real specs)" do
-      get root_path
-      expect(response).to have_http_status(200)
-    end
-  end
+		it "can view page when signed out" do
+			get root_path
+			expect(response).to have_http_status(200)
+		end
+
+		it "can view page when signed in" do
+			sign_in FactoryGirl.create(:user)
+			get root_path
+			expect(response).to have_http_status(200)
+		end
+	end
 end
